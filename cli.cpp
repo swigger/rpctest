e57a65476265dc9b78c85a1104ec0462da342ef2
xungeng <xungeng@gmail.com>
2017-10-26 01:46:12 +0800

    demo

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,34 @@
 #include "stdafx.h"
+#include "kms_i.h"
+#include <string>
+using std::string;
+
 
 int main()
 {
+	RPC_WSTR cpstr = 0;
+	RpcStringBindingCompose(NULL, (RPC_WSTR)L"ncacn_ip_tcp", (RPC_WSTR)L"127.0.0.1" /*NULL*/, (RPC_WSTR)L"1689", NULL, &cpstr);
+	RpcBindingFromStringBinding(cpstr, &kms_netHandle);
+
+	string req("hello");
+	unsigned char * r = 0;
+	int rsz = 0;
+	int vv = RequestActivation((int)req.size(), (BYTE*) req.c_str(), &rsz, &r);
+	printf("s=%s\nrv=%d\n", r, vv);
+	
+	midl_user_free(r);
+	RpcStringFree(&cpstr);
+	RpcBindingFree(&kms_netHandle);
+
 	return 0;
+}
+
+void * midl_user_allocate(size_t len)
+{
+	return(malloc(len));
+}
+
+void midl_user_free(void __RPC_FAR *ptr)
+{
+	free(ptr);
 }
--- srv.cpp
+++ srv.cpp
@@ -1,6 +1,36 @@
 #include "stdafx.h"
+#include "kms_i.h"
+
+extern "C"
+int RequestActivation(int requestSize, unsigned char *request, int *responseSize, unsigned char **response)
+{
+	const char * rr = "world";
+	*response = (unsigned char*) strdup(rr);
+	*responseSize = (int) strlen(rr) + 1;
+	return 0;
+}
+
+void Shutdown(void)
+{
+	RpcMgmtStopServerListening(NULL);
+	RpcServerUnregisterIf(NULL, NULL, FALSE);
+}
 
 int main()
 {
+	//RpcServerUseProtseqEp("ncacn_np", 20, "//pipe//unique.name", NULL);
+	RpcServerUseProtseqEp((RPC_WSTR)L"ncacn_ip_tcp", RPC_C_PROTSEQ_MAX_REQS_DEFAULT, (RPC_WSTR)L"1689", NULL);
+	RpcServerRegisterIfEx(KMSServer_v1_0_s_ifspec, NULL, NULL, RPC_IF_ALLOW_CALLBACKS_WITH_NO_AUTH, RPC_C_LISTEN_MAX_CALLS_DEFAULT, NULL);
+	RpcServerListen(0, RPC_C_LISTEN_MAX_CALLS_DEFAULT, 0/*block*/);
 	return 0;
+}
+
+void * midl_user_allocate(size_t len)
+{
+	return(malloc(len));
+}
+
+void midl_user_free(void __RPC_FAR *ptr)
+{
+	free(ptr);
 }
